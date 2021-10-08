67186eb8fe501bb66dce4fc996919abe121004d9
Sebastien Colbert <colbertsebastien@gmail.com>
2021-10-08 11:41:19 +0200

    Desktop Style

--- src/App.js
+++ src/App.js
@@ -15,11 +15,29 @@ const Main = styled.main`
   @media (min-width: 800px) {
     margin: 3rem auto;
     padding: 3rem;
-    width: 80%;
+    width: 90%;
     height: 100%;
     border: solid #333 3px;
     box-shadow: 4px 4px 8px 2px #000000;
     background: var(--bg-main-color);
+  }
+`
+
+const Container = styled.div`
+  @media (min-width: 800px) {
+    display: flex;
+  }
+`
+
+const Col1 = styled.div`
+  @media (min-width: 800px) {
+    width: calc(100% / 3 * 2);
+  }
+`
+
+const Col2 = styled.div`
+  @media (min-width: 800px) {
+    width: calc(100% / 3);
   }
 `
 
@@ -29,11 +47,17 @@ function App () {
       <GlobalStyle />
       <Main>
         <Header data={DataIdentity} />
-        <Experiences data={DataExperiences} />
-        <Formations data={DataFormations} />
-        <Contact data={DataIdentity} />
-        <Competences data={DataCompetences} />
-        <QRCode />
+        <Container>
+          <Col1>
+            <Experiences data={DataExperiences} />
+            <Formations data={DataFormations} />
+          </Col1>
+          <Col2>
+            <Contact data={DataIdentity} />
+            <Competences data={DataCompetences} />
+            <QRCode />
+          </Col2>
+        </Container>
       </Main>
     </>
   )
